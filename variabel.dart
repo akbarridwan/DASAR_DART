89f62ff252a23b097379fefe3ed9aeb21224d3fe
AkbarRidwan7 <78518227+AkbarRidwan7@users.noreply.github.com>
2021-02-18 19:50:57 +0700

    Add files via upload

---
+++ variabel.dart
@@ -0,0 +1,15 @@
+void main(){
+    // membaut variabel dengan tipe data
+    String nama = "Akbar";
+    int umur = 23;
+    double tinggi = 175.43;
+    bool isMenikah = false;
+
+    // membuat veriabel dengan kata kunci var
+    var alamat = "Sibolga, Indonesia";
+
+    // mencetak variabel
+    print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm.");
+    print("Menikah: $isMenikah");
+    print("Alamat: $alamat");
+}
